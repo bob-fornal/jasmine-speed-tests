f6bb8b5a2e6dd45a8d29e0fb7e5cbe14a806b3e5
Bob Fornal <bob.fornal@Bobs-MacBook-Pro.local>
2019-09-23 12:56:53 -0400

    cleaned .2 tests

--- spec/_multipleIt.2.spec.js
+++ spec/_multipleIt.2.spec.js
@@ -1,6 +1,4 @@
 describe('speed testing', function() {
-    const maxRuns = 10000;
-  
     describe('multiple its per describe 1000', function() {
       describe('wrapper', function() {
         it('something simple', function() {
--- spec/_singleIt.2.spec.js
+++ spec/_singleIt.2.spec.js
@@ -1,6 +1,4 @@
 describe('speed testing', function() {
-    const maxRuns = 10000;
-  
     describe('single it per describe 1000', function() {
       describe('single it run', function() {
         it('something simple', function() {
